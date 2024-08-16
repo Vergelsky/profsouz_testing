68f7ed99caf6183be76e45d271c0475e0a415cee
Vergelsky <graf.werger@gmail.com>
2024-08-16 14:55:25 +0700

    Извлечение данных настроил

---
+++ profsouz_testing/profsouz_test/main.py
@@ -0,0 +1,25 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def parse_python_versions(playwright: Playwright) -> None:
+    data = []
+
+    with playwright.chromium.launch(headless=True) as browser:
+        with browser.new_context() as context:
+            with context.new_page() as page:
+                page.goto("https://www.python.org/downloads/")
+                table = page.locator("div.download-list-widget li").all()
+                for string in table:
+                    string_data = {
+                        "Release version": string.locator('span.release-number a').text_content(),
+                        "Release date": string.locator('span.release-date').text_content(),
+                        "Download": string.locator('span.release-download a').text_content(),
+                        "Release Notes": string.locator('span.release-enhancements a').text_content()
+                        }
+                    data.append(string_data)
+                page.close()
+    print(data)
+
+with sync_playwright() as playwright:
+    parse_python_versions(playwright)
