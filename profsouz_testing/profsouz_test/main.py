827b79db2842c66cf97b3b26afc9cf1270c6f4e2
Vergelsky <graf.werger@gmail.com>
2024-08-16 15:59:26 +0700

    Сохранение в таблицу настроил

--- profsouz_testing/profsouz_test/main.py
+++ profsouz_testing/profsouz_test/main.py
@@ -1,8 +1,8 @@
-import re
+import pandas
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def parse_python_versions(playwright: Playwright) -> None:
+def parse_python_versions(playwright: Playwright):
     data = []
 
     with playwright.chromium.launch(headless=True) as browser:
@@ -14,12 +14,19 @@ def parse_python_versions(playwright: Playwright) -> None:
                     string_data = {
                         "Release version": string.locator('span.release-number a').text_content(),
                         "Release date": string.locator('span.release-date').text_content(),
-                        "Download": string.locator('span.release-download a').text_content(),
-                        "Release Notes": string.locator('span.release-enhancements a').text_content()
-                        }
+                        "Download": string.locator('span.release-download a').get_attribute('href'),
+                        "Release Notes": string.locator('span.release-enhancements a').get_attribute('href')
+                    }
                     data.append(string_data)
                 page.close()
+    return data
+
+
+def write_to_exel(data):
+    data = pandas.DataFrame(data)
     print(data)
+    data.to_excel('Python releases by version number.xlsx', index=False)
+
 
 with sync_playwright() as playwright:
-    parse_python_versions(playwright)
+    write_to_exel(parse_python_versions(playwright))
